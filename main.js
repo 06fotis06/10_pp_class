33da0b631026e6982c734fa9465fd6d63a55d85d
06fotis06 <83125818+06fotis06@users.noreply.github.com>
2023-05-04 13:16:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+	$('.menu-burger__header').click(function(){
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+	});
+});
